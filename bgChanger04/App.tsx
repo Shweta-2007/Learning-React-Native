5a435368682606433f53578b941563eb4ab04d54
Shweta Sinha <sinhashweta713@gmail.com>
2024-03-04 17:01:55 +0530

    project04

---
+++ bgChanger04/App.tsx
@@ -0,0 +1,52 @@
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+
+export default function App() {
+  const [randomBackground, setRandomBackground] = useState('#ffffff');
+  const generateColor = () => {
+    const hexRange = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.round(Math.random() * 16)];
+    }
+    setRandomBackground(color);
+  };
+
+  return (
+    <>
+      <StatusBar backgroundColor={randomBackground} />
+      <View style={[styles.container, {backgroundColor: randomBackground}]}>
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn}>
+            <Text style={styles.actionBtnText}>Press me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  actionBtn: {
+    borderRadius: 12,
+    backgroundColor: '#6A1B40',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+  actionBtnText: {
+    fontSize: 24,
+    color: 'white',
+    textTransform: 'uppercase',
+  },
+});
